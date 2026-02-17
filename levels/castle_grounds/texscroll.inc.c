f2c7d6e3f4fb6517a84a3525752fb2f24a42dc9b
SCharryaB3 <drenokisdetermination@gmail.com>
2026-02-17 11:42:14 +0300

    wf level + skybox being mean

--- levels/castle_grounds/texscroll.inc.c
+++ levels/castle_grounds/texscroll.inc.c
@@ -1,11 +1,11 @@
-void scroll_castle_grounds_dl_Plane_mesh_layer_1_vtx_21() {
+void scroll_castle_grounds_dl_Plane_mesh_layer_1_vtx_19() {
 	int i = 0;
 	int count = 18;
 	int width = 32 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
-	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_Plane_mesh_layer_1_vtx_21);
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_Plane_mesh_layer_1_vtx_19);
 
 	deltaX = (int)(0.4399999976158142 * 0x20) % width;
 
@@ -41,6 +41,6 @@ void scroll_castle_grounds_dl_Plane_mesh_layer_5_vtx_1() {
 }
 
 void scroll_castle_grounds() {
-	scroll_castle_grounds_dl_Plane_mesh_layer_1_vtx_21();
+	scroll_castle_grounds_dl_Plane_mesh_layer_1_vtx_19();
 	scroll_castle_grounds_dl_Plane_mesh_layer_5_vtx_1();
 };
--- levels/castle_grounds/texscroll.inc.h
+++ levels/castle_grounds/texscroll.inc.h
@@ -1,3 +1,3 @@
-extern void scroll_castle_grounds_dl_Plane_mesh_layer_1_vtx_21();
+extern void scroll_castle_grounds_dl_Plane_mesh_layer_1_vtx_19();
 extern void scroll_castle_grounds_dl_Plane_mesh_layer_5_vtx_1();
 extern void scroll_castle_grounds();
